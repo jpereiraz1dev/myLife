2778160600f596d3618c14af2e44053719c9cf8c
unknown <jpereiraz.dev@gmail.com>
2026-06-08 20:30:33 -0300

    add category to task model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+choices_category = [
+    ("PHYSICAL","Physical"),
+    ("PROG","Prog"),
+    ("ENGLISH","English"),
+    ("COLLEGE","College"),
+]
+
 class Product(models.Model):
     name = models.CharField(max_length=20,null=True,blank=False)
     desc = models.TextField(null=False,blank=False)
@@ -9,6 +16,7 @@ class Product(models.Model):
 
 class Task(models.Model):
     name = models.CharField(max_length=20,null=True,blank=False)
+    category = models.CharField(max_length=40,choices=choices_category,blank=False,null=True)
     desc = models.TextField(null=False,blank=False)
     points = models.IntegerField()
     duration = models.TimeField()
