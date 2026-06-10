15e8a92cef674a88fe4ee11092e29bfff8d1cbf4
unknown <jpereiraz.dev@gmail.com>
2026-06-09 21:46:08 -0300

    alterando models e admin do app store

--- store/admin.py
+++ store/admin.py
@@ -6,7 +6,6 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ("name","price","public")
     list_filter = ("name","price","public")
     search_fields = ("name","price","public")
-    readonly_fields = ("public",)
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ("name","points","duration")
--- store/models.py
+++ store/models.py
@@ -1,24 +1,30 @@
 from django.db import models
-
-choices_category = [
-    ("PHYSICAL","Physical"),
-    ("PROG","Prog"),
-    ("ENGLISH","English"),
-    ("COLLEGE","College"),
-]
+from datetime import timedelta
+from myAuth.models import MyUser
 
 class Product(models.Model):
+    user = models.ForeignKey(MyUser,on_delete=models.SET_NULL,null=True,blank=True)
     name = models.CharField(max_length=20,null=True,blank=False)
     desc = models.TextField(null=False,blank=False)
     price = models.IntegerField()
     public = models.BooleanField(default=True)
     
 
+class CategoryTask(models.Model):
+    name = models.CharField(max_length=20,null=False,blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
-    name = models.CharField(max_length=20,null=True,blank=False)
-    category = models.CharField(max_length=40,choices=choices_category,blank=False,null=True)
+    user = models.ForeignKey(MyUser,on_delete=models.SET_NULL,null=True,blank=True)
+    name = models.CharField(max_length=20,null=True,blank=False,)
     desc = models.TextField(null=False,blank=False)
+    category = models.ForeignKey(CategoryTask,blank=False,null=True, on_delete=models.SET_NULL)
     points = models.IntegerField()
-    duration = models.TimeField()
+    duration = models.TimeField(default = timedelta(minutes=30))
     done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
